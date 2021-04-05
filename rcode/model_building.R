6ae1907a890e1726af3af8898e9723ef207722d3
Shaun Cass <shauncassdata@gmail.com>
2021-04-04 17:54:59 -0700

    add graphs for training data

--- rcode/model_building.R
+++ rcode/model_building.R
@@ -14,3 +14,33 @@ train_data <- training(data_split)
 test_data <- testing(data_split)
 
 
+
+# Looking at a few important graphs again with the training data this time
+# instead of the complete data.
+
+train_data %>%
+  select(price, carat, depth, table, x, y, z) %>%
+  drop_na() %>%
+  ggcorr(label = TRUE) +
+  ggtitle("Pearson Correlation of Continuous Variables in Training Data")
+
+train_data %>%
+  select(price, carat, depth, table, x, y, z) %>%
+  drop_na() %>%
+  pairs(main = "Scatterplots of Continuous Variables in Training Data")
+
+# log scale all of the numerical variables
+train_data %>%
+  select(price, carat, depth, table, x, y, z) %>%
+  drop_na() %>%
+  mutate_all(log) %>%
+  pairs(main = "Scatterplots of the log of Continuous Variables in Training Data")
+
+
+
+
+  
+  
+  
+  
+
